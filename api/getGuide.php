f012d91a70b080fd4fae8a592d9a052fc56abf7f
Dave Hulbert <dave1010@gmail.com>
2023-07-04 20:17:06 +0100

    Split out guides for specific tasks

--- api/getGuide.php
+++ api/getGuide.php
@@ -2,6 +2,11 @@
 
 $guide = file_get_contents(__DIR__ . '/../the-guide.txt');
 
+$otherGuides = scandir(__DIR__ . '/../guides');
+array_shift($otherGuides); // .
+array_shift($otherGuides); // ..
+
 echo json_encode([
-    'guide' => $guide
+    'guide' => $guide,
+    'other-guides' => $otherGuides,
 ]);
